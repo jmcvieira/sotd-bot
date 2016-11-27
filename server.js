3619b04e33cd434fd5581987c4d65d9312a164c8
João Vieira <joao.vieira@syone.com>
2016-11-27 15:58:47 +0000

    Test

--- server.js
+++ server.js
@@ -1,20 +1,46 @@
 var TelegramBot = require('node-telegram-bot-api');
+var shuffle = require('shuffle-array');
+var clone = require('clone');
 
 var token = '258556381:AAHNbf6mJZkahlsFPHW2pexDtsJ7saDneSc';
 // Setup polling way
-var bot = new TelegramBot(token, {polling: true});
+var bot = new TelegramBot(token, {
+  polling: true
+});
 
-// Matches /echo [whatever]
-bot.onText(/\/echo (.+)/, function (msg, match) {
+users = ['Fernando', 'Vieira', 'Tiago', 'Caetano', 'Soraia'];
+currentList = clone(users);
+previousList = [];
+currentOrder = 'SOTD - A ordem desta semana é:\nSegunda-Feira - ' + currentList[0] + '\nTerça-Feira - ' + currentList[1] +
+  '\nQuarta-Feira - ' + currentList[2] + '\nQuinta-Feira - ' + currentList[3] + '\nSexta-Feira - ' + currentList[4];
+
+bot.onText(/\/shuffle/, function (msg) {
   var fromId = msg.from.id;
-  var resp = match[1];
+
+  previousList = clone(currentList);
+  shuffle(currentList);
+  currentOrder = 'SOTD - A ordem desta semana é:\nSegunda-Feira - ' + currentList[0] + '\nTerça-Feira - ' + currentList[1] +
+    '\nQuarta-Feira - ' + currentList[2] + '\nQuinta-Feira - ' + currentList[3] + '\nSexta-Feira - ' + currentList[4];
+  var resp = currentOrder;
+
   bot.sendMessage(fromId, resp);
 });
 
-// Any kind of message
-bot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-  // photo can be: a file path, a stream or a Telegram file_id
-  var photo = 'cats.png';
-  bot.sendPhoto(chatId, photo, {caption: 'Lovely kittens'});
+bot.onText(/\/list/, function (msg) {
+  var fromId = msg.from.id;
+
+  var resp = currentOrder;
+
+  bot.sendMessage(fromId, resp);
+});
+
+bot.onText(/\/reset/, function (msg) {
+  var fromId = msg.from.id;
+
+  currentList = clone(users);
+  currentOrder = 'SOTD - A ordem desta semana é:\nSegunda-Feira - ' + currentList[0] + '\nTerça-Feira - ' + currentList[1] +
+    '\nQuarta-Feira - ' + currentList[2] + '\nQuinta-Feira - ' + currentList[3] + '\nSexta-Feira - ' + currentList[4];
+  var resp = 'A ordem original foi reposta!';
+
+  bot.sendMessage(fromId, resp);
 });
